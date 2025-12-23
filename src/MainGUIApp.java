2bc41a26e9e899b4d721a7a04cb5e56a41d7d27e
harishy0406 <harivelgm76@gmail.com>
2025-12-23 19:40:33 +0530

    GUI based UI

--- src/MainGUIApp.java
+++ src/MainGUIApp.java
@@ -4,7 +4,7 @@ import com.cityelectro.ui.LandingPage;
 import com.cityelectro.ui.MainGUI;
 
 /**
- * Main GUI Application - Entry point for CityElectro GUI version
+ * Main GUI Application - Entry point for CityElectro GUI version 
  */
 public class MainGUIApp {
     public static void main(String[] args) {
